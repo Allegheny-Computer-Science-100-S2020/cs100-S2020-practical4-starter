5e823f25ca14a6578bdf0b04345688a22eb62fea
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-02-07 07:44:27 -0500

    Add the template version of the MadLibs program.

---
+++ src/main/java/practicalfour/MadLibs.java
@@ -0,0 +1,33 @@
+package practicalfour;
+
+// TODO: Add the correct imports at the top of the program
+
+import java.util.Date;
+import java.util.Scanner;
+
+// TODO: Make sure to add all of the correctly formatted JavaDoc comments
+//
+// NOTE: Another name for JavaDocument comments is multiple-line comments
+
+public class MadLibs {
+
+  public static void main(String[] args) {
+    // display the name of the programmer and the date
+    System.out.println("Add Your Name Here" + new Date() + "\n");
+    // declare the starting file and scanner
+    File wordInputsFile = null;
+    Scanner scanner = null;
+    // TODO: Step Zero: Construct the scanner that is connected to the file
+    // NOTE: this is the location of the input file
+    // ├── input
+    //     └── madlibs_inputs.txt
+    // NOTE: Refer to a previous assignment for a suitable try-catch block
+
+    // TODO Step One: Read in and display the words and numbers in a story
+
+    // TODO: Step Two: Display the word problem using the input words
+
+    // TODO: Step Three: Display a final "thankyou" message
+  }
+
+}
